8da8d6d553bf06ae2bdfd98471c3c5188261548d
DeK <nofatclips@gmail.com>
2013-07-05 21:20:31 +0200

    Changed the RegExp to detect a word: using word boundary instead of non word character.

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ editorErrante.filter("spaziaturaMancante", function() {
         };
     };
 }).filter("parolaMancante", function() {
-    var theWord = function(word) {return new RegExp("\\W" + word + "\\W|^" + word + "\\W", "i");}
+    var theWord = function(word) {return new RegExp("\\b" + word + "\\b", "i");}
     return function(word, theText) {
         if (!word) return "";
         if (theText.search(theWord(word)) === -1) {
